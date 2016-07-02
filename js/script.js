5124eac6767ed605cbe67d4265e344c3c7d42933
Ian Kitchen <ian@oceanrain.com>
2016-07-02 14:03:10 +0100

    initial revision

---
+++ js/script.js
@@ -0,0 +1,38 @@
+
+
+$(document).ready(function(){
+	$("#design").click(function(){
+		window.location.href = "http://design.oceanrain.com/";
+	});
+});
+
+$(document).ready(function(){
+	$("#oceanrain").click(function(){
+		window.location.href = "http://www.oceanrain.com/";
+	});
+});
+
+$(document).ready(function(){
+	$("#oceanrainBreadcrumb").click(function(){
+		window.location.href = "http://www.oceanrain.com/";
+	});
+});
+
+$(document).ready(function(){
+	$("#calendar").click(function(){
+		window.location.href = "http://calendar.oceanrain.com/";
+	});
+});
+
+$(document).ready(function(){
+	$("#photos").click(function(){
+		window.location.href = "http://www.oceanrain.com/photos.html";
+	});
+});
+
+$(document).ready(function(){
+	$("#Group").click(function(){
+		window.location.href = "http://www.oceanrain.com/Onboard.php";
+	});
+});
+
